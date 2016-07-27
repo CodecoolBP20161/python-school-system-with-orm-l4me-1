271de43a54e1813147d4ec5bf1f49a89f9994eb8
rekan <rekanagy06@gmail.com>
2016-07-27 11:52:40 +0200

    partial implementation - School example data

--- example_data.py
+++ example_data.py
@@ -1,3 +1,11 @@
 # This script can generate example data for "City" and "InterviewSlot" models.
 
 from models import *
+from applicant import *
+from school import *
+from city import *
+
+# SCHOOL example data
+miskolc2016 = School.insert(location='Miskolc')
+budapest2016 = School.insert(location='Budapest')
+krakow2016 = School.insert(location='Krakow')
