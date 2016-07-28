1f066c73001cf88158666419bf6932df81ba17b8
Mate Gonczi <gamte19@gmail.com>
2016-07-28 11:05:38 +0200

    Implement assign interview to new applicant method

--- interview.py
+++ interview.py
@@ -2,6 +2,11 @@ from models import *
 from interview_slot import *
 from applicant import *
 
+
 class Interview(BaseModel):
     application_code = ForeignKeyField(Applicant, to_field='application_code', related_name='interview')
     interview_detail = ForeignKeyField(InterviewSlot, related_name='interview')
+
+    @classmethod
+    def assign_interview_to_new_applicant(cls, app, slot):
+        cls.create(application_code=app, interview_detail=slot)
