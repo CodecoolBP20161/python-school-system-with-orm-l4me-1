3e2daac3098642e47a89f0b80f54261a4822813f
Balazs Ando <elnesia@gmail.com>
2016-07-28 18:40:43 +0200

    Automated database build

--- models.py
+++ models.py
@@ -3,7 +3,16 @@ from peewee import *
 # Configure your database connection here
 # database name = should be your username on your laptop
 # database user = should be your username on your laptop
+
 db = PostgresqlDatabase('school_system')
+
+
+def connect_to_db():
+    try:
+        db.connect()
+        return True
+    except:
+        return False
 
 
 class BaseModel(Model):
