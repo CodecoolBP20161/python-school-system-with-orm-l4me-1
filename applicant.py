35d5fbd287f679e1a08fe068b5035d24ad66ad7a
Mate Gonczi <gamte19@gmail.com>
2016-07-28 10:28:19 +0200

    Modified staticmethod to classmethod

--- applicant.py
+++ applicant.py
@@ -13,9 +13,9 @@ class Applicant(Person):
     status = IntegerField(choices=[(0, 1, 2, 3), ('new', 'in progress', 'rejected', 'accepted')])
     application_code = CharField(null=True, unique=True)
 
-    @staticmethod
-    def applicants_without_applicant_code():
-        query = Applicant.select().where(Applicant.application_code >> None)
+    @classmethod
+    def applicants_without_applicant_code(cls):
+        query = cls.select().where(cls.application_code >> None)
         if query:
             print("The following applicants have no application code: \n")
             for applicant in query:
@@ -29,12 +29,12 @@ class Applicant(Person):
             generated = ''.join([random.choice(abc[abs(i)//2]) for i in range(-5, 6, 2)]) + "#&"
         Applicant.update(application_code=generated, status=1).where(Applicant.id == self.id).execute()
 
-    @staticmethod
-    def find_closest_school():
-        query = Applicant.select().where(Applicant.school >> None)
+    @classmethod
+    def find_closest_school(cls):
+        query = cls.select().where(cls.school >> None)
         if query:
             print("The following applicants have no school connected: \n")
             for applicant in query:
                 print(applicant.first_name, applicant.last_name)
                 school = City.get(City.name == applicant.location).school
-                Applicant.update(school=school).where(Applicant.id == applicant.id).execute()
+                cls.update(school=school).where(cls.id == applicant.id).execute()
