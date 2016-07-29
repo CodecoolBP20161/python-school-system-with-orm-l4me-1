8c7dce09f61acf496c5a8240d14086896efff551
rekan <rekanagy06@gmail.com>
2016-07-29 09:58:52 +0200

    no records found feature added

--- applicant.py
+++ applicant.py
@@ -42,41 +42,66 @@ class Applicant(Person):
 
     @classmethod
     def filter_applicant_by_school(cls, school):
-        for applicant in [applicant for applicant in cls.select().where(cls.school == school)]:
-            print(applicant.full_name + ": " + applicant.school.location)
+        query = cls.select().where(cls.school == school)
+        if query:
+            for applicant in query:
+                print(applicant.full_name + ": " + applicant.school.location)
+        else:
+            print('No records found')
 
     @classmethod
     def filter_applicant_by_status(cls, status):
         status_codes = {0: 'new', 1: 'in progress', 2: 'rejected', 3: 'accepted'}
-        in_this_status = False
-        for applicant in [applicant for applicant in cls.select().where(cls.status == status)]:
-            print(applicant.full_name + ": " + status_codes[applicant.status])
-            in_this_status = True
-        if in_this_status == False:
-            print("Nobady is in this status")
+        query = cls.select().where(cls.status == status)
+        if query:
+            for applicant in query:
+                print(applicant.full_name + ": " + status_codes[applicant.status])
+        else:
+            print('No records found')
 
     @classmethod
     def filter_applicant_by_location(cls, location):
-        for applicant in [applicant for applicant in cls.select().where(cls.location == location)]:
-            print(applicant.full_name + ": " + applicant.location)
+        query = cls.select().where(cls.location == location)
+        if query:
+            for applicant in query:
+                print(applicant.full_name + ": " + applicant.location)
+        else:
+            print('No records found')
 
     @classmethod
     def filter_applicant_by_name(cls, name):
         query = cls.select().where(cls.first_name.startswith(name) | (cls.last_name.startswith(name)))
-        for applicant in query:
-            print(applicant.full_name)
+        if query:
+            for applicant in query:
+                print(applicant.full_name)
+        else:
+            print('No records found')
 
     @classmethod
     def filter_applicant_by_email(cls, email):
-        for applicant in [applicant for applicant in cls.select().where(cls.email.contains(email))]:
-            print(applicant.full_name + ": " + applicant.email)
+        query = cls.select().where(cls.email.contains(email))
+        if query:
+            for applicant in [applicant for applicant in cls.select().where(cls.email.contains(email))]:
+                print(applicant.full_name + ": " + applicant.email)
+        else:
+            print('No records found')
 
     @classmethod
-    def detailes_of_applicant(cls, email, pw):
+    def filter_applicant_by_time(cls, from_time, to_time):
+        query = cls.select().where((cls.time >= from_time) & (cls.time <= to_time))
+        if query:
+            for applicant in query:
+                print(str(applicant.time) + ': ' + applicant.full_name)
+        else:
+            print('No records found')
+
+    @classmethod
+    def details_of_applicant(cls, email, pw):
         status_codes = {0: 'new', 1: 'in progress', 2: 'rejected', 3: 'accepted'}
         applicant_code = input("Enter your applicant code: ")
         for applicant in [applicant for applicant in cls.select().where(cls.email == email)]:
             if applicant.application_code == applicant_code:
-                print("\nYour status is "+status_codes[applicant.status], "\nYour school is in " + applicant.school.location)
+                print("\nYour status is "+status_codes[applicant.status], "\nYour school is in " +
+                      applicant.school.location)
             else:
                 print("This code is not existing in the database, try again later :)")
